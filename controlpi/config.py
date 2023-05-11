24b9e635d0cfc1e6ba77ea76128ad841c2a28ed9
Massimo Biagioli <biagiolimassimo@gmail.com>
2023-05-11 10:55:20 +0200

    Add config

---
+++ controlpi/config.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    database_url: str
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    settings = Settings()
+    return settings
